b25595c35a9ee256bd45911c9af712f5b0b060af
Bhavanaraj12 <bhavana@chaavie.com>
2024-12-04 19:02:10 +0530

    added pharmacy quotation apis

--- routes/pharmacyquotation/pharmacyquotation.routes.js
+++ routes/pharmacyquotation/pharmacyquotation.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { assignpharmacy,getpackedorders } = require("./pharmacyquotation.controller");
+const { assignpharmacy,getpackedorders,getpharmacies,getproductspharmacy } = require("./pharmacyquotation.controller");
 
 const pharmacyquotationRouter = express.Router()
 pharmacyquotationRouter.post("/assignpharmacy", assignpharmacy);
 pharmacyquotationRouter.get("/getpackedorders",getpackedorders)
-
+pharmacyquotationRouter.post("/getpharmacies",getpharmacies)
+pharmacyquotationRouter.post("/getproductsph",getproductspharmacy)
 module.exports = pharmacyquotationRouter;
