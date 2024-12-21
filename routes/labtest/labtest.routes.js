34d126c27464c7d8ffaf8d77b8277bf31392d22a
Bhavanaraj12 <bhavana@chaavie.com>
2024-12-21 23:11:58 +0530

    lab add,get details,add package,get package,add cart,get cart,delete cart apis

--- routes/labtest/labtest.routes.js
+++ routes/labtest/labtest.routes.js
@@ -4,13 +4,26 @@ const {
   labtestadd,
   getlabtests,
   package_add,
-  getpackagetests
+  getpackagetests,
+  labadd,
+  getlabs,
+  lab_profile,
+  testToCart,
+  gettestCart,
+  removeTestFromCart
 } = require("./labtest.controller");
 const LabtestRouter = express.Router();
+const auth = require("../../middleware/Auth/auth");
 
 LabtestRouter.post("/labtestadd", labtestadd);
 LabtestRouter.get("/getlabtests", getlabtests);
 LabtestRouter.post("/package_add", package_add);
 LabtestRouter.get("/getpackagetests", getpackagetests);
+LabtestRouter.post("/labadd",labadd)
+LabtestRouter.get("/getlabs",getlabs)
+LabtestRouter.post("/lab_profile",lab_profile)
+LabtestRouter.post("/testToCart",auth,testToCart)
+LabtestRouter.get("/gettestCart",auth,gettestCart)
+LabtestRouter.post("/removeTestFromCart",auth,removeTestFromCart)
 
 module.exports = LabtestRouter;
