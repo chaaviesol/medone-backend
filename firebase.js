15e5edeb1bc799c72bbf4bf4d2a98ae83b678e8b
Bhavanaraj12 <bhavana@chaavie.com>
2025-01-27 11:40:52 +0530

    updates

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const admin = require('firebase-admin')
 ///git//
-// const serviceAccount = require('../med-one/firebase-adminsdk.json')
+const serviceAccount = require('../med-one/firebase-adminsdk.json')
 //local///////////////
-const serviceAccount = require('../medone-backend/firebase-adminsdk.json')
+// const serviceAccount = require('../medone-backend/firebase-adminsdk.json')
 require("dotenv").config();
 
 
